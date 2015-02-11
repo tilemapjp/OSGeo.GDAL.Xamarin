f2bea3b017bb09c71f72c8e953a808f51e115c90
OHTSUKA Ko-hei <kochizufan@gmail.com>
2015-02-11 11:39:49 +0900

    There are no warp API on gdal SWIG bindings.. orz

--- OSGeo.GDAL.Test.iOS/Create.cs
+++ OSGeo.GDAL.Test.iOS/Create.cs
@@ -2,6 +2,8 @@
 using System;
 using NUnit.Framework;
 using System.IO;
+using OSGeo.GDAL;
+using OSGeo.OGR;
 
 namespace OSGeo.GDAL.Test
 {
@@ -37,6 +39,36 @@ namespace OSGeo.GDAL.Test
 		}
 
 		[Test]
+		public void CreateFile ()
+		{
+			var documents =
+				System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+			var dstFileName = System.IO.Path.Combine(documents, "create.tif");
+			var dstDS = driver.Create (dstFileName, 512, 512, 1, DataType.GDT_Byte, null);
+
+			dstDS.SetGeoTransform (new double[6]{ 444720, 30, 0, 3751320, 0, -30 });
+
+			var srs = new OSR.SpatialReference (null);
+			srs.SetUTM (11, 1);
+			srs.SetWellKnownGeogCS ("NAD27");
+			string wkt;
+			srs.ExportToWkt (out wkt);
+			dstDS.SetProjection (wkt);
+
+			var raster = new byte[512 * 512];
+
+			dstDS.GetRasterBand (1).WriteRaster (
+				0, 0, 512, 512, raster, 512, 512, 0, 0);
+
+			if (dstDS != null) {
+				dstDS.Dispose ();
+				dstDS = null;
+			}
+
+			Assert.True(File.Exists(dstFileName));
+		}
+			
+		[Test]
 		public void CreateCopy ()
 		{
 			var documents =
@@ -46,8 +78,18 @@ namespace OSGeo.GDAL.Test
 				File.Delete (dstFileName);
 				
 			var srcDS = Gdal.Open (StartPhase.BogotaTiffPath, Access.GA_ReadOnly);
+			var srcTiled = srcDS.GetMetadataItem ("TILED", "IMAGE_STRUCTURE");
+			var srcComp = srcDS.GetMetadataItem ("COMPRESSION", "IMAGE_STRUCTURE");
+			//Assert.AreEqual (null, srcTiled);
+			Assert.AreEqual (null, srcComp);
 
-			var dstDS = driver.CreateCopy (dstFileName, srcDS, 0, null, null, null);
+			var dstDS = driver.CreateCopy (dstFileName, srcDS, 0, 
+				new string[2]{"TILED=YES","COMPRESS=PACKBITS"}, 
+				null, null);
+			var dstTiled = dstDS.GetMetadataItem ("TILED", "IMAGE_STRUCTURE");
+			var dstComp = dstDS.GetMetadataItem ("COMPRESSION", "IMAGE_STRUCTURE");
+			//Assert.AreEqual ("YES", dstTiled);
+			Assert.AreEqual ("PACKBITS", dstComp);
 
 			if (dstDS != null) {
 				dstDS.Dispose ();
---
+++ OSGeo.GDAL.Test.iOS/Warp.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using NUnit.Framework;
+using OSGeo.GDAL;
+
+namespace OSGeo.GDAL.Test
+{
+	[TestFixture]
+	public class Warp
+	{
+		[SetUp]
+		public void Setup ()
+		{
+		}
+
+		[TearDown]
+		public void Tear ()
+		{
+		}
+
+		[Test]
+		public void WarpTest ()
+		{
+			var srcDS = Gdal.Open (StartPhase.BogotaTiffPath, Access.GA_ReadOnly);
+			var documents =
+				System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+			var dstFileName = System.IO.Path.Combine(documents, "warpout.tif");
+			var dstDS = Gdal.Open (dstFileName, Access.GA_Update);
+
+		}
+	}
+}
